6ec677804dc9d747b0d8df6d6e612861b14b861f
Fatlys <143842013+Fatlys@users.noreply.github.com>
2025-12-16 23:56:56 -0500

    Update maplibre.js

--- Tp2/src/js/maplibre.js
+++ Tp2/src/js/maplibre.js
@@ -17,42 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     "frieza": { lng: -46.6333, lat: -23.5505 }
   };
 
-    // CREATION MARQUEUR POUR PERSONNAGES
-  map.on("load", () => {
-    Object.entries(locations).forEach(([id, loc]) => {
-      const el = document.createElement("div");
-      el.className = id === "goku" ? "map-marker active" : "map-marker";
-      el.innerHTML = '<div class="marker-dot"></div>';
-      
-      const marker = new maplibregl.Marker({ element: el, anchor: "center" })
-        .setLngLat([loc.lng, loc.lat])
-        .addTo(map);
-      
-      markers[id] = { marker, element: el };
-    });
-  });
-
-  const style = document.createElement("style");
-  style.textContent = `
-    .map-marker { width: 20px; height: 20px; position: relative; }
-    .marker-dot { 
-      position: absolute; 
-      top: 50%; left: 50%; 
-      transform: translate(-50%, -50%);
-      width: 8px; height: 8px;
-      background: #00ff6a; 
-      border-radius: 50%;
-      box-shadow: 0 0 10px #00ff6a;
-    }
-    .map-marker.active .marker-dot { 
-      width: 12px; height: 12px; 
-      box-shadow: 0 0 20px #00ff6a; 
-    }
-  `;
-  document.head.appendChild(style);
-
-  
-
 
  
 });
